12cb75dca8a8967199431a10a2ece29dfbf1599a
Mark Zuneska <mzuneska@gmail.com>
2010-07-04 20:04:22 -0400

    Added actions using command pattern.

---
+++ Munchkin/Lib/Actions/KillPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Munchkin.Lib.Actions
+{
+    class KillPlayer : IAction
+    {
+        private Player player;
+
+        public KillPlayer(Player player) 
+        {
+            this.player = player;
+        }
+
+        public void Execute()
+        {
+            player.Die();
+        }
+
+    }
+}
